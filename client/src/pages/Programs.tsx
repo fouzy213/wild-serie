a651bcc31c037d019c04c778e15ec79a7b23bb23
fouzy chikouche <fouzy_69@hotmail.fr>
2025-01-06 10:19:48 +0100

    quetes_5

--- client/src/pages/Programs.tsx
+++ client/src/pages/Programs.tsx
@@ -1,27 +1,36 @@
-import { useEffect,useState } from "react";
-interface ProgramsProps{
-    id:number;
-    title:string;
-    poster:string;
+import { useEffect, useState } from "react";
+interface ProgramsProps {
+  id: number;
+  title: string;
+  poster: string;
+  synopsis: string;
+  year: number;
+  country: string;
 }
-const Programs =() => {
-const [programs,setPrograms]=useState<ProgramsProps[]>([])
+const Programs = () => {
+  const [programs, setPrograms] = useState<ProgramsProps[]>([]);
 
-useEffect(() => {
-fetch("http://localhost:3310/api/programs")
-.then((res) => res.json())
-.then((data) => setPrograms(data))
-.catch((err) => console.error(err))
-},[]);
+  useEffect(() => {
+    fetch("http://localhost:3310/api/programs")
+      .then((res) => res.json())
+      .then((data) => setPrograms(data))
+      .catch((err) => console.error(err));
+  }, []);
 
-return (
-<>
-<main>
-    {programs.map((program)=>(
-        <img src={program.poster} alt="film" />
-    ))}
-</main>
-</>
-)
-}
- export default Programs;
+  return (
+    <>
+      {programs.map((programDetail) => (
+        <article key={programDetail.id}>
+          <h3>{programDetail.title}</h3>
+          <img src={programDetail.poster} alt={programDetail.title} />
+          <p>
+            {programDetail.synopsis}
+            {programDetail.year}
+            {programDetail.country}
+          </p>
+        </article>
+      ))}
+    </>
+  );
+};
+export default Programs;
--- server/src/router.ts
+++ server/src/router.ts
@@ -8,17 +8,17 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
-import sayActions from "./modules/sayActions";
 import programActions from "./modules/program/programActions";
+import sayActions from "./modules/sayActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 import categoryActions from "./modules/categoryActions";
 router.get("/api/categories", categoryActions.browse);
-router.get("/api/categories/:id",categoryActions.read);
+router.get("/api/categories/:id", categoryActions.read);
 /* ************************************************************************* */
-router.get("/api/programs",programActions.browse);
+router.get("/api/programs", programActions.browse);
 
-router.get("/",sayActions.sayWelcome);
+router.get("/", sayActions.sayWelcome);
 
 export default router;
